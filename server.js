0bbca3b4b57a935fc11e2c916fa9fe05bc3155bf
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2020-12-01 23:28:25 -0500

    ready for deployment

--- server.js
+++ server.js
@@ -1,17 +1,66 @@
-
+require('dotenv').config()
 const express = require("express")
-const morgan = require("morgan")
 const app = express()
+const cors = require("cors")
+const helmet = require("helmet")
+const morgan = require("morgan")
+const POKEDEX = require('./pokedex.json')
 
-app.use(morgan('dev'))
 
-app.use((req, res) => {
-    res.send("hellorr world")
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+
+app.use(helmet())
+
+app.use(cors())
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => { 
 })
 
-const PORT = 8000
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
-    
+
+
+app.use(function validateBearerToken(req, res, next){
+    const bearerToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN
+    if (!bearerToken || bearerToken.split(" ")[1] !== apiToken){
+        return res.status(401).json({error: 'Unauthorized Request'})
+    }
+    next()
 })
+
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+  
+
+function handleGetTypes(req, res){
+res.json(validTypes)
+}
+
+app.get("/types", handleGetTypes)
+
+app.get("/pokemon", handleGetPokemon)
+
+function handleGetPokemon(req, res){
+    let {name="", type} = req.query
+    if (type !== undefined && !validTypes.includes(type)){
+        return res.status(400).send("Please provide a valid type")
+    }
+    const results = POKEDEX.pokemon.filter(current => {
+       let includesName = current.name.toLowerCase().includes(name.toLowerCase())
+       let includesType = type === undefined ? true: current.type.map(current => current.toLowerCase()).includes(type.toLowerCase())
+       return includesName && includesType
+    })
+
+    res.send(results)
+}
